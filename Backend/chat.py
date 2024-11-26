6e7d66a96ff8b09dfacdc6f35550c322dd14d6f7
Sunnoogo77 <162154376+Sunnoogo77@users.noreply.github.com>
2024-11-26 23:53:29 +0100

    Update chat.py

--- Backend/chat.py
+++ Backend/chat.py
@@ -1,13 +1,35 @@
-from flask_socketio import SocketIO, emit, join_room, leave_room
-from app import socketio
+from flask_socketio import SocketIO, emit, join_room, leave_room, emit
+from Backend.app1 import socketio
 
-@socketio.on('join_room')
+socketio = SocketIO()
+
+#@socketio.on('join_room')
+#def handle_join_room_event(data):
+ #   app.logger.info(f"{data['username']} joined room {data['room']}")
+  #  join_room(data['room'])
+   # emit('join_room_announcement', data, to=data['room'])
+
+#@socketio.on('send_message')
+#def handle_send_message_event(data):
+ #   app.logger.info(f"{data['username']} sent message to room {data['room']}: {data['message']}")
+  #  emit('receive_message', data, to=data['room'])
+
+@socketio.on("join_room")
 def handle_join_room_event(data):
-    app.logger.info(f"{data['username']} joined room {data['room']}")
-    join_room(data['room'])
-    emit('join_room_announcement', data, to=data['room'])
+    username = data["username"]
+    room = data["room"]
+    join_room(room)
+    emit("message", {"message": f"{username} a rejoint la salle {room}."}, to=room)
 
-@socketio.on('send_message')
-def handle_send_message_event(data):
-    app.logger.info(f"{data['username']} sent message to room {data['room']}: {data['message']}")
-    emit('receive_message', data, to=data['room'])
+@socketio.on("leave_room")
+def handle_leave_room_event(data):
+    username = data["username"]
+    room = data["room"]
+    leave_room(room)
+    emit("message", {"message": f"{username} a quitté la salle {room}."}, to=room)
+
+@socketio.on("message")
+def handle_message_event(data):
+    room = data["room"]
+    message = data["message"]
+    emit("message", {"message": message}, to=room)
