6c2ec5807931390b23924e2fc1532e3d6c9f7ad7
Sunnoogo77 <162154376+Sunnoogo77@users.noreply.github.com>
2024-12-23 14:18:07 +0000

    Update chat.py

--- Backend/chat.py
+++ Backend/chat.py
@@ -3,17 +3,6 @@ from Backend.app1 import socketio
 
 socketio = SocketIO()
 
-#@socketio.on('join_room')
-#def handle_join_room_event(data):
- #   app.logger.info(f"{data['username']} joined room {data['room']}")
-  #  join_room(data['room'])
-   # emit('join_room_announcement', data, to=data['room'])
-
-#@socketio.on('send_message')
-#def handle_send_message_event(data):
- #   app.logger.info(f"{data['username']} sent message to room {data['room']}: {data['message']}")
-  #  emit('receive_message', data, to=data['room'])
-
 @socketio.on("join_room")
 def handle_join_room_event(data):
     username = data["username"]
